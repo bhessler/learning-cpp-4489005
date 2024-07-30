21a0a21c3383e7063c3bd1f5a4bed917dee88f6f
Bella H <112017777+bhessler@users.noreply.github.com>
2024-07-30 23:13:46 +0000

    Add more code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World
+
+#include <iostream>
+
+int main(){
+    //count = character output reps the out put window that text is sent to
+    //endl = end line
+    std::cout << "Hi There!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
