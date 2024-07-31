2679a07bd3c5b68bf673bee4bd5ae29ee2e3c52d
Bella H <112017777+bhessler@users.noreply.github.com>
2024-07-31 18:03:58 +0000

    notes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -6,7 +6,8 @@
 
 int main(){
     //count = character output reps the out put window that text is sent to
-    //endl = end line
+    //endl = end line aka: break line
+    // the \n also works as break line
     std::cout << "Hi There!" << std::endl;
     std::cout << std::endl << std::endl;
     return(0);
